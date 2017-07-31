0438a9e073d19c3a814c36dd33d8a004f8966638
glazar <gabriel.lazar@3pillarglobal.com>
2017-07-31 14:41:12 +0300

    Add initial tests for enums

--- src/files/file1.ts
+++ src/files/file1.ts
@@ -1,2 +1,14 @@
 export const square = (x: number) => x * x;
 export const cube = (x: number) => x * x * x;
+
+export enum ColorEnum {
+  Red = "RED",
+  Green = "GREEN",
+  Blue = "BLUE"
+}
+
+export enum NumberEnum {
+  Zero = 0,
+  One = 1,
+  Two = 2
+}
--- src/files/spec/file1.spec.ts
+++ src/files/spec/file1.spec.ts
@@ -1,4 +1,4 @@
-import { cube, square } from "../file1";
+import { cube, square, ColorEnum, NumberEnum } from "../file1";
 
 describe("file1", () => {
   it("should square numbers", () => {
@@ -7,5 +7,17 @@ describe("file1", () => {
 
   it("should cube numbers", () => {
     expect(cube(3)).toBe(27);
+  });
+
+  it("should expose color enum", () => {
+    expect(ColorEnum.Red).toBe("RED");
+    expect(ColorEnum.Green).toBe("GREEN");
+    expect(ColorEnum.Blue).toBe("BLUE");
+  });
+
+  it("should expose number enum", () => {
+    expect(NumberEnum.Zero).toBe(0);
+    expect(NumberEnum.One).toBe(1);
+    expect(NumberEnum.Two).toBe(2);
   });
 });
